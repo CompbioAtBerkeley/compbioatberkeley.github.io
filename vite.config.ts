84ee7ccbf80cd6fa0d9d47e1b601af1c75d64f5f
Qile0317 <qile0317@gmail.com>
2026-02-06 14:08:16 -0800

    fixes to static rendering

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
@@ -22,3 +23,4 @@ export default defineConfig(({ mode }) => ({
     },
   }
 }));
+
