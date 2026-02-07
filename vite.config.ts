33546b9331e15aaeb496f105834770bcd5550dc9
Qile0317 <qile0317@gmail.com>
2026-02-06 16:25:06 -0800

    add GA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -16,6 +17,11 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
     ViteImageOptimizer(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-Z4Z237WBGY',
+      }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -23,4 +29,3 @@ export default defineConfig(({ mode }) => ({
     },
   }
 }));
-
